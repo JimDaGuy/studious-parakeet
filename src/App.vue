3354226b483f9bc518127e71178e74abb2fa4bcb
James Digrazia <jtd2401@rit.edu>
2019-03-16 18:12:25 -0400

    Setup vue project with KeenUI components

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ui-button @click="openModal('modal1')">Basic Modal</ui-button>
+    <ui-modal ref="modal1" title="Basic Modal">Hi World~~~</ui-modal>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  methods: {
+    openModal(ref) {
+      this.$refs[ref].open();
+    }
+  },
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 100%;
+}
+</style>
